73b3c197f8931630a3dd7a8f498a31292356038e
fr30 <fjakub.iia@gmail.com>
2024-06-21 14:44:04 +0200

    Fix training and preprocessing

--- scripts/chunk_data.py
+++ scripts/chunk_data.py
@@ -8,54 +8,62 @@ from merlin.schema.tags import Tags
 from transformers4rec.utils.data_utils import save_time_based_splits
 
 
-INPUT_DATA_DIR = "data/ebnerd_small_modified"
+INPUT_DATA_DIR = "data/ebnerd_demo_modified"
 OUTPUT_DIR = os.path.join(INPUT_DATA_DIR, "sessions_by_ts")
 PADDING = False
-PART_SIZE = "128MB"  # Change it to "1GB" if you have enough memory, it will be faster
+PART_SIZE = "256MB"  # Change it to "1GB" if you have enough GPU mem, it will be faster
 
 if not os.path.exists(f"{INPUT_DATA_DIR}/all_sorted.parquet"):
     df = pd.read_parquet(f"{INPUT_DATA_DIR}/all.parquet")
-    df.sort_values(by="impression_ts")
-    df["impression_ts"] = (datetime.datetime.now() - df["impression_ts"]).dt.days
-    df["impression_ts"] = df["impression_ts"] - df["impression_ts"].min()
+    df.sort_values(by="impression_time")
+    df["impression_time"] = (datetime.datetime.now() - df["impression_time"]).dt.days
+    df["impression_time"] = df["impression_time"] - df["impression_time"].min()
     df.to_parquet(f"{INPUT_DATA_DIR}/all_sorted.parquet")
     del df
 
-SESSIONS_MAX_LENGTH = 10
+# df = pd.read_parquet(f"{INPUT_DATA_DIR}/all.parquet")
+# df.sort_values(by="impression_time")
+# df["impression_time"] = (datetime.datetime.now() - df["impression_time"]).dt.days
+# df["impression_time"] = df["impression_time"] - df["impression_time"].min()
+# df.to_parquet(f"{INPUT_DATA_DIR}/all_sorted.parquet")
+# del df
+
+SESSIONS_MAX_LENGTH = 20
 
 # Categorify categorical features
 categ_feats = [
     "article_id",
-    "article_is_premium",
+    "is_premium",
     "article_type",
-    "article_category",
+    "category",
 ] >> nvt.ops.Categorify()
 
 # Define Groupby Workflow
 groupby_feats = categ_feats + [
-    "user_id",
+    "session_id",
     "article_emb_id",
-    "impression_ts",
-    "article_read_time",
-    "article_total_read_time",
-    "article_sentiment",
-    "article_ctr",
+    "impression_time",
+    "read_time",
+    # "article_total_read_time",
+    "sentiment_score",
+    # "article_ctr",
 ]
 
 # Group interaction features by session
 groupby_features = groupby_feats >> nvt.ops.Groupby(
-    groupby_cols=["user_id"],
+    groupby_cols=["session_id"],
     aggs={
         "article_id": ["list", "count"],
+        # "session_id": ["list"],
         "article_emb_id": ["list"],
-        "article_is_premium": ["list"],
+        "is_premium": ["list"],
         "article_type": ["list"],
-        "article_category": ["list"],
-        "article_read_time": ["list"],
-        "article_total_read_time": ["list"],
-        "article_sentiment": ["list"],
-        "article_ctr": ["list"],
-        "impression_ts": ["min"],
+        "category": ["list"],
+        "read_time": ["list"],
+        # "article_total_read_time": ["list"],
+        "sentiment_score": ["list"],
+        # "article_ctr": ["list"],
+        "impression_time": ["min"],
     },
     name_sep="-",
 )
@@ -67,18 +75,16 @@ sequence_features_truncated_item = (
 )
 
 sequence_features_truncated_cat = (
-    groupby_features[
-        "article_is_premium-list", "article_type-list", "article_category-list"
-    ]
+    groupby_features["is_premium-list", "article_type-list", "category-list"]
     >> nvt.ops.ListSlice(-SESSIONS_MAX_LENGTH, pad=PADDING)
     >> nvt.ops.AddMetadata(tags=[Tags.CATEGORICAL])
 )
 sequence_features_truncated_cont = (
     groupby_features[
-        "article_read_time-list",
-        "article_total_read_time-list",
-        "article_sentiment-list",
-        "article_ctr-list",
+        "read_time-list",
+        # "article_total_read_time-list",
+        "sentiment_score-list",
+        # "article_ctr-list",
     ]
     >> nvt.ops.ListSlice(-SESSIONS_MAX_LENGTH, pad=PADDING)
     >> nvt.ops.AddMetadata(tags=[Tags.CONTINUOUS])
@@ -94,7 +100,7 @@ sequence_features_truncated_emb = (
 # Filter out sessions with length 1 (not valid for next-item prediction training and evaluation)
 MINIMUM_SESSION_LENGTH = 2
 selected_features = (
-    groupby_features["article_id-count", "impression_ts-min", "user_id"]
+    groupby_features["article_id-count", "impression_time-min", "session_id"]
     + sequence_features_truncated_item
     + sequence_features_truncated_cat
     + sequence_features_truncated_cont
@@ -109,25 +115,25 @@ seq_feats_list = (
     filtered_sessions[
         "article_id-list",
         "article_emb_id-list",
-        "article_is_premium-list",
+        "is_premium-list",
         "article_type-list",
-        "article_category-list",
-        "article_read_time-list",
-        "article_total_read_time-list",
-        "article_sentiment-list",
-        "article_ctr-list",
+        "category-list",
+        "read_time-list",
+        # "article_total_read_time-list",
+        "sentiment_score-list",
+        # "article_ctr-list",
     ]
     >> nvt.ops.ValueCount()
 )
 
 print("Creating workflow...")
 workflow = nvt.Workflow(
-    filtered_sessions["user_id", "impression_ts-min"] + seq_feats_list
+    filtered_sessions["session_id", "impression_time-min"] + seq_feats_list
 )
 
 print("Creating dataset...")
 dataset = nvt.Dataset(
-    os.path.join(INPUT_DATA_DIR, "all_sorted.parquet"), part_size="128MB"
+    os.path.join(INPUT_DATA_DIR, "all_sorted.parquet"), part_size=PART_SIZE
 )
 
 # Generate statistics for the features and export parquet files
@@ -144,8 +150,8 @@ print("Creating time based splits...")
 save_time_based_splits(
     data=nvt.Dataset(sessions_gdf),
     output_dir=OUTPUT_DIR,
-    partition_col="impression_ts-min",
-    timestamp_col="user_id",
+    partition_col="impression_time-min",
+    timestamp_col="session_id",
     cpu=False,
 )
 print("Done!")
